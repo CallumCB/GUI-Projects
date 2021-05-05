3153f121817a794b4aeee5fd2801e7cc932cc5e0
CallumCB <77706780+CallumCB@users.noreply.github.com>
2021-05-05 09:13:07 -0600

    Update tkinterTest.py

--- tkinterTest.py
+++ tkinterTest.py
@@ -1,8 +1,22 @@
 from tkinter import *
 top = Tk()
+playList=[]
+def results():
+    result=E1.get()
+    print(result)
+    print(type(result))
+def addToList():
+    newItem = E1.get()
+    playList.append(newItem)
 L1 = Label(top, text="Hello, world")
 L1.grid(column= 0, row= 1)
+
 E1 = Entry(top, bd = 5)
 E1.grid(column= 0, row= 2)
 
+B1 = Button(text= "  Get data  ", bg="green", command=results)
+B1.grid(column= 0, row= 3)
+
+B2 = Button(text = "Add to List", bg= "red", command =addToList)
+B2.grid(column=2, row=2)
 top.mainloop()
